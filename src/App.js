1dddd253e4eebe5b52148d03d4aa7254eff27c4e
Generic35 <ricklamontagne@gmail.com>
2025-09-20 23:10:03 -0400

    feat: scaffold the App and Navigation components

--- src/App.js
+++ src/App.js
@@ -13,12 +13,50 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [dappazon, setDappazon] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
+
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
+  const loadBlockchainData = async () => {
+    // TODO: implement loadBlockchainData here
+    // TODO: implement loading of account here
+    // TODO: immplement on account change here
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
 
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
 
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
+      )}
     </div>
   );
 }
